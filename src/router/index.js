4de890b0ffcca23ff81f49f32319dcd6d72f965a
Mark Stoumann <markstoumann@Marks-Laptop.local>
2022-11-23 11:24:22 +0100

    Moved graphs out into their own page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Testing from '../views/Testing.vue'
+import Graphs from '../views/Graphs.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/testing',
       name: 'testing',
       component: Testing
+    },
+    {
+      path: '/graphs',
+      name: 'graphs',
+      component: Graphs
     },
   ]
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,16 @@
 <template>
-	<div class="fixed inset-0 grid grid-cols-3 grid-rows-1 gap-10 bg-slate-900 text-slate-100 p-10 box-border">
-		<div class="col-span-1 grid grid-rows-2 gap-10 overflow-hidden">
-			<div>
+	<div class="fixed inset-0 grid grid-cols-2 grid-rows-1 gap-10 bg-slate-900 text-slate-100 p-10 box-border">
+		<div class="col-span-1 grid grid-rows-3 gap-10 overflow-hidden">
+			<div class="row-span-2">
 				<SolarPanels></SolarPanels>
 			</div>
 			<div class="bg-slate-800 rounded-lg p-10 flex justify-center items-center relative overflow-hidden outline-text">
 				<CurrentTime></CurrentTime>
 			</div>
 		</div>
-		<div class="grid col-span-2 grid-cols-3 grid-rows-2 gap-10">
-			<div class="col-span-2 bg-slate-800 rounded-lg flex flex-col justify-between relative overflow-hidden p-10">
-				<span class="text-[2vw] uppercase tracking-wide">Luftkvalitet i Mindfuture</span>
-				<ClimateData></ClimateData>
-			</div>
+		<div class="grid grid-rows-2 gap-10">
 			<div class="bg-slate-800 rounded-lg p-10 flex flex-col justify-between relative overflow-hidden row-span-2">
 				<Weather />
-			</div>
-			<div class="bg-slate-800 col-span-2 rounded-lg p-10 flex flex-col justify-between relative overflow-hidden">
-				<span class="text-[2vw] uppercase tracking-wide">Dagens elpriser</span>
-				<PriceRecords></PriceRecords>
 			</div>
 		</div>
 	</div>
