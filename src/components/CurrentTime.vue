afa750c09bd8f77e2e7f61a5f353e4bb46e68054
Mark Stoumann <markstoumann@Marks-Laptop.lan>
2022-12-06 15:40:42 +0100

    Changing time

--- src/components/CurrentTime.vue
+++ src/components/CurrentTime.vue
@@ -1,16 +1,17 @@
 <template>
-    <div class="text-[8vw] break-words leading-none tracking-widest text-left font-bold">{{state.hour}}<span class="transition-all duration-1000 ease-in-out" :class="{'text-transparent': !state.blinkOn}">:</span>{{state.minutes}}</div>
+    <div class="text-[8vw] break-words leading-none tracking-widest text-left font-bold">{{state.hour}}<span class="transition-all duration-500 ease-in-out" :class="{'text-transparent': !state.blinkOn}">:</span>{{state.minutes}}<span class="transition-all duration-500 ease-in-out" :class="{'text-transparent': !state.blinkOn}">:</span>{{state.seconds}}</div>
 </template>
 
 <script setup>
     import { reactive } from 'vue';
 
-    const state = reactive({ hour: '', minutes: '', blinkOn: false });
+    const state = reactive({ hour: '', minutes: '', seconds: '', blinkOn: false });
 
     function updateTime () {
         let today = new Date();
         state.hour = ('0' + today.getHours().toLocaleString('da-DK')).slice(-2);
         state.minutes = ('0' + today.getMinutes().toLocaleString('da-DK')).slice(-2);
+        state.seconds = ('0' + today.getSeconds().toLocaleString('da-DK')).slice(-2);
         state.blinkOn = !state.blinkOn;
     }
 
