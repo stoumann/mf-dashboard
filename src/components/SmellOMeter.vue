8c2a5a80db87f1538359247d66aabd9660f7f307
Mark Stoumann <mark@stoumann.dk>
2023-08-17 09:15:20 +0200

    New threshold

--- src/components/SmellOMeter.vue
+++ src/components/SmellOMeter.vue
@@ -2,7 +2,7 @@
     <div class="bg-black rounded-lg p-10 flex flex-col relative overflow-hidden text-[2vw] tracking-wider h-full w-full space-y-4">
         <div>Smell-O-Meter</div>
         <div v-if="state.sensorData" class="text-[1.2vw]">
-            <div v-if="state.sensorData.co2eq < 470" class="bg-green-600 text-white p-2 px-4 rounded-lg text-center">#1 - Good to go!</div>
+            <div v-if="state.sensorData.co2eq < 666" class="bg-green-600 text-white p-2 px-4 rounded-lg text-center">#1 - Good to go!</div>
             <div v-else class="bg-red-600 text-white p-2 px-4 rounded-lg text-center">#1 - Be careful!</div>
             <!-- {{ state.sensorData.co2eq }} - {{ state.sensorData.tvoc }} -->
         </div>
