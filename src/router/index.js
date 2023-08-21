c97866b8da8bc558c2779f79adf42f74dd7b5f40
Mark Stoumann <mark@stoumann.dk>
2023-08-21 08:39:57 +0200

    Added mini page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Testing from '../views/Testing.vue'
 import Graphs from '../views/Graphs.vue'
+import Mini from '../views/Mini.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +22,11 @@ const router = createRouter({
       name: 'graphs',
       component: Graphs
     },
+    {
+      path: '/mini',
+      name: 'mini',
+      component: Mini
+    }
   ]
 })
 
---
+++ src/views/Mini.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="fixed inset-0 grid grid-cols-2 grid-rows-2 gap-10 bg-zinc-900 text-white box-border p-[2vw]">
+        <div>
+            <div class="gap-10 flex flex-col">
+                <TodaysMenu class="!text-[3vw]" />
+                <div class="bg-black rounded-lg p-10 flex flex-col justify-between relative overflow-hidden row-span-3">
+                    <Weather />
+                </div>
+            </div>
+        </div>
+        <div>
+            <div class="gap-10 flex flex-col">
+                <PrusaJobs />
+                <SmellOMeter />
+            </div>
+        </div>
+    </div>
+
+    <LunchQueue />
+</template>
+
+<script setup>
+    import LunchQueue from '../components/LunchQueue.vue';
+    import TodaysMenu from '../components/TodaysMenu.vue';
+	import SmellOMeter from '../components/SmellOMeter.vue';
+	import PrusaJobs from '../components/PrusaJobs.vue';
+    import Weather from '../components/Weather.vue';
+</script>
