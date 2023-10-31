4c9bd4621c0e350b066c9f56dc47b661f934e2fd
Mark Stoumann <mark@stoumann.dk>
2023-10-31 15:31:51 +0100

    Removed Jannick

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,10 @@
 	<div class="fixed inset-0 grid grid-cols-3 grid-rows-1 gap-10 bg-zinc-900 text-white p-24 box-border">
 		<div class="col-span-2 grid grid-rows-2 gap-10 overflow-hidden">
 			<!-- <SolarPanels /> -->
-			<img src="@/assets/Jannick.jpg" alt="" class="object-cover h-full w-full rounded-lg object-top" />
+			<!-- <img src="@/assets/Jannick.jpg" alt="" class="object-cover h-full w-full rounded-lg object-top" /> -->
+			<div class="w-full h-full bg-black rounded-lg flex items-center justify-center text-[2vw]">
+				What should we have here?
+			</div>
 
 			<div class="grid grid-cols-3 gap-10">
 				<div class="gap-10 flex flex-col">
